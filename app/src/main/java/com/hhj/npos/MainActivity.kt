c0fd01e12fda69d56679d5be6ab2cc8c46c52fec
한현종 <hanhyeonjong@hanhyeonjong-ui-MacBookAir.local>
2023-03-23 17:58:57 +0900

    기초세팅

--- app/src/main/java/com/hhj/npos/MainActivity.kt
+++ app/src/main/java/com/hhj/npos/MainActivity.kt
@@ -2,10 +2,16 @@ package com.hhj.npos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hhj.npos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //뷰바인딩
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
